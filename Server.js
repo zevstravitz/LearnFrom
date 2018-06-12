ac12feb3d5453a22b9e0c199e2bcc6606cb5a4cf
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2018-06-11 20:07:54 -0500

    deploy

--- Server.js
+++ Server.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var app     = express();
-var path    = require("path");
+const express = require("express");
+const app     = express();
+const path    = require("path");
 
+app.use(express.static(path.join(__dirname, 'public'))); // STATIC ASSETS
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(8080);
+console.log("Running at Port 8080");
