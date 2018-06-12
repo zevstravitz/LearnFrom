ef55952a05649d6552ca235a7a3fc07f3d1b2b2b
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2018-06-11 20:23:52 -0500

    deploy

--- Server.js
+++ Server.js
@@ -10,5 +10,5 @@ app.get('/',function(req,res){
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
